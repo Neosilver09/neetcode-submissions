e96cff622c4e08bda74e8b83a6fb83984534862b
Neosilver09 <45509652+Neosilver09@users.noreply.github.com>
2026-09-11 15:02:02 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dico = {')':'(', '}':'{',']':'['}
+        for char in s:
+            if char == ')' or char == '}' or char == ']' :
+                check = stack.pop() if len(stack) != 0 else 0
+                if check != dico.get(char):
+                    return False
+            else:
+                stack.append(char)
+        return not stack
+
+                
+
+        
