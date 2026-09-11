974b0b6327bd15af81a2d57b91c7e33b7ea7e866
Neosilver09 <45509652+Neosilver09@users.noreply.github.com>
2026-09-11 14:39:49 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t) :
+            return False
+
+        count = {}
+        # ["r":2]
+        for caract in s:
+            count[caract]=count.get(caract,0) + 1
+
+        for caract in t:
+            caractCount = count.get(caract)
+            if caractCount:
+                count[caract] -= 1
+        
+        for caractCount in count.values():
+            if caractCount != 0:
+                return False
+
+        return True
+            
+
+        
+            
+            
+        
